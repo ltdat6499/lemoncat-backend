b8920f82290bcc629331392b8430440daf202006
Ly Thanh Dat <dat6499proo@gmail.com>
2021-05-07 08:40:58 +0700

    feat: acl init

--- middlewares/rbac/index.js
+++ middlewares/rbac/index.js
@@ -1,4 +1,125 @@
-// const requireText = require("require-text");
-// const { RBAC } = require("rbac");
-// const rbac = new RBAC(JSON.parse(requireText("./secure.json", require)));
-// await rbac.init();
+const NodeAcl = require("acl");
+const acl = new NodeAcl(new NodeAcl.memoryBackend(), {
+  debug: (msg) => {
+    console.log("-DEBUG-", msg);
+  },
+});
+
+acl.allow([
+  {
+    roles: "s-admin",
+    allows: [
+      {
+        resources: "users",
+        permissions: "delete",
+      },
+    ],
+  },
+  {
+    roles: "admin",
+    allows: [
+      {
+        resources: "movies",
+        permissions: "*",
+      },
+      {
+        resources: "persons",
+        permissions: "*",
+      },
+      {
+        resources: "productions",
+        permissions: "*",
+      },
+      {
+        resources: "tops",
+        permissions: "*",
+      },
+      {
+        resources: "category",
+        permissions: "*",
+      },
+      {
+        resources: "posts",
+        permissions: "*",
+      },
+      {
+        resources: "comments",
+        permissions: "*",
+      },
+    ],
+  },
+  {
+    roles: "s-user",
+    allows: [
+      {
+        resources: "posts",
+        permissions: "createSneakShow",
+      },
+    ],
+  },
+  {
+    roles: "user",
+    allows: [
+      {
+        resources: "categories",
+        permissions: "get",
+      },
+      {
+        resources: "tops",
+        permissions: "get",
+      },
+      {
+        resources: "productions",
+        permissions: "get",
+      },
+      {
+        resources: "posts",
+        permissions: "create",
+      },
+      {
+        resources: "posts",
+        permissions: "update",
+      },
+      {
+        resources: "posts",
+        permissions: "delete",
+      },
+      {
+        resources: "persons",
+        permissions: "get",
+      },
+      {
+        resources: "movies",
+        permissions: "get",
+      },
+      {
+        resources: "comments",
+        permissions: "*",
+      },
+      {
+        resources: "users",
+        permissions: "get",
+      },
+    ],
+  },
+  {
+    roles: "guest",
+    allows: [
+      {
+        resources: "users",
+        permissions: "create",
+      },
+      {
+        resources: "users",
+        permissions: "update",
+      },
+    ],
+  },
+]);
+
+acl.addRoleParents("user", "guest");
+acl.addRoleParents("s-user", "user");
+acl.addRoleParents("admin", "s-user");
+acl.addRoleParents("s-admin", "admin");
+
+module.exports = acl;
