f8d4063fef1a63570defe25637267802121344a0
Ly Thanh Dat <dat6499proo@gmail.com>
2021-07-05 16:57:57 +0700

    add es export

--- plugins/im-exports.js
+++ plugins/im-exports.js
@@ -1,6 +1,7 @@
-const knex = require("../controllers/knex");
 const fs = require("fs");
 const readline = require("readline");
+const knex = require("../controllers/knex");
+const tools = require("../global");
 
 const exportDatabase = async (table, limit) => {
   limit = limit || 500000;
@@ -24,11 +25,6 @@ const exportDatabase = async (table, limit) => {
   console.log("Done");
 };
 
-// exportDatabase("users");
-// exportDatabase("actions");
-// exportDatabase("persons");
-// exportDatabase("flims");
-// exportDatabase("posts", 2000);
 const loadArray = async (name) => {
   const file = fs.createReadStream(name);
 
@@ -42,6 +38,7 @@ const loadArray = async (name) => {
   }
   return results.reverse();
 };
+
 const importDatabase = async () => {
   const files = await fs.readdirSync("./");
   for (const file of files.filter((item) => item.includes(".txt"))) {
@@ -53,7 +50,109 @@ const importDatabase = async () => {
   console.log("ALL DONE");
 };
 
-importDatabase();
+const esExportPersons = async (table, limit) => {
+  limit = limit || 5000;
+  let counter = 0;
+  let results = [1, 2];
+  while (results.length) {
+    results = await knex(table)
+      .select("id", "name", "summary", "slug", "born_in", "birth", "images")
+      .limit(limit)
+      .offset(counter * limit);
+    results = results.map((result) => {
+      result.images = result.images[0];
+      return result;
+    });
+    if (results.length) {
+      await fs.appendFileSync(
+        `./database/${table}_${counter}.txt`,
+        JSON.stringify(results),
+        "utf8"
+      );
+      console.log(counter * limit);
+      counter++;
+    } else break;
+  }
+  console.log("Done");
+};
+
+const esExportFlims = async (table, limit) => {
+  limit = limit || 5000;
+  let counter = 0;
+  let results = [1, 2];
+  while (results.length) {
+    results = await knex(table)
+      .select("id", "type", "info", "streamings", "data", "slug")
+      .limit(limit)
+      .offset(counter * limit);
+    results = results.map((item) => {
+      return {
+        id: item.id,
+        type: item.type,
+        name: item.info.name,
+        summary: item.info.summary,
+        tags: item.info.tags.join(", "),
+        genres: item.info.genres.join(", "),
+        rating: item.info.rating,
+        poster: item.info.poster,
+        collection: item.info.collection,
+        productions: item.info.productions.join(", "),
+        theatersDate: item.info.theaters_date,
+        streamings: item.streamings.join(", "),
+        lemonScore: item.data.rotten_tomatoes.tomatometer_score,
+        slug: item.slug,
+      };
+    });
+    if (results.length) {
+      await fs.appendFileSync(
+        `./database/${table}_${counter}.txt`,
+        JSON.stringify(results),
+        "utf8"
+      );
+      console.log(counter * limit);
+      counter++;
+    } else break;
+  }
+  console.log("Done");
+};
+
+const esExportPosts = async (table, limit) => {
+  limit = limit || 5000;
+  let counter = 0;
+  let results = [1, 2];
+  while (results.length) {
+    results = await knex(table)
+      .select("id", "title", "content", "data", "tags", "slug")
+      .where({ type: "news" })
+      .limit(limit)
+      .offset(counter * limit);
+    results = results.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        content: tools.htmlToText(item.content),
+        section: item.data.section,
+        tags: item.tags.join(", "),
+        poster: item.data.preview_poster,
+        slug: item.slug,
+      };
+    });
+    if (results.length) {
+      await fs.appendFileSync(
+        `./database/${table}_${counter}.txt`,
+        JSON.stringify(results),
+        "utf8"
+      );
+      console.log(counter * limit);
+      counter++;
+    } else break;
+  }
+  console.log("Done");
+};
+
+// esExportPersons("persons", 15000);
+// esExportFlims("flims", 1000);
+// esExportPosts("posts", 400);
 module.exports = {
   exportDatabase,
   importDatabase,
