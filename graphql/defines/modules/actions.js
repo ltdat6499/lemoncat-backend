9355eecacb66ae4d46c5fbbf58ff737adaefbf1d
Ly Thanh Dat <dat6499proo@gmail.com>
2021-07-17 01:14:05 +0700

    update graphql

--- graphql/defines/modules/actions.js
+++ graphql/defines/modules/actions.js
@@ -1,5 +1,7 @@
 const controllers = require("../../../controllers");
 const tools = require("../../../global");
+const jwt = require("../../../middlewares/jwt");
+const configs = require("../../../configs");
 
 module.exports = {
   Query: {
@@ -14,10 +16,37 @@ module.exports = {
   },
   Mutation: {
     async updateAction(__, args) {
-      let { input, id } = args;
-      input = tools.changeCaseType(input, "snakeCase");
-      const [result] = await controllers.update("actions", id, input);
-      return result;
+      let { input } = args;
+      // input = tools.changeCaseType(input, "snakeCase");
+      const auth = jwt.verify(input.token, configs.signatureKey);
+      if (!auth.data.id)
+        return {
+          result: {},
+          error: auth.err,
+        };
+      let data = {
+        type: input.type,
+        parent: input.parent,
+        parent_type: input.parentType,
+        uid: auth.data.id,
+        data: input.data,
+      };
+      let result = {};
+      if (input.action === "update")
+        result = await controllers.update("actions", input.id, data);
+      else if (input.action === "create")
+        result = await controllers.create("actions", data);
+      else if (input.action === "delete")
+        result = await controllers.deleteById("actions", input.id);
+      else
+        return {
+          result: {},
+          error: "invalid action verb",
+        };
+      return {
+        result: result[0],
+        error: "",
+      };
     },
   },
   Action: {
